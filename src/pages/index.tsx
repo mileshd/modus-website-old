85c29cdc5e1277f6a2a9f012a1a60429da8a1536
Miles Dickinson <miles.h.dickinson@gmail.com>
2020-07-27 11:25:39 -0400

    Improve styling of Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -52,43 +52,45 @@ const Index = ({ data, location }) => {
         </div>
         <div className="clients">
           <div className="container">
+            <h1>Brands Growing with Us</h1>
             <div className="row clients__logos">
-              <div className="col-xs-12 col-sm-4">
-                 <Image
-                   fixed={data.enlightenedLogo.childImageSharp.fixed}
-                 />
+              <div className="col-xs-12 col-sm-6 col-md-3">
+                <img className="logo-picture" src="https://media-exp1.licdn.com/dms/image/C4D0BAQE9ykVEomLipg/company-logo_200_200/0?e=2159024400&v=beta&t=Xg5k3btpp82qdsturaBcNTF1AiCECQoaGXjAok_iSbk"/>
               </div>
-              <div className="col-xs-12 col-sm-4">
-                 <Image
-                   fixed={data.banzaLogo.childImageSharp.fixed}
-                 />
+              <div className="col-xs-12 col-sm-6 col-md-3">
+                <img className="logo-picture" src="https://i.pinimg.com/originals/39/6d/74/396d74d91507e5f2d3f5b2418407e0a6.jpg"/>
               </div>
-              <div className="col-xs-12 col-sm-4">
-                 <Image
-                   fixed={data.huKitchenLogo.childImageSharp.fixed}
-                 />
+              <div className="col-xs-12 col-sm-6 col-md-3">
+                <img className="logo-picture" src="https://s3-media0.fl.yelpcdn.com/bphoto/mO14VxIohwkp_aoj2AG_HQ/348s.jpg"/>
               </div>
-            </div>
-            <div className="clients__quote">
-              <div className="quote-text">
-                Modus is is best thing to happen to our brand. We have never been more confident and powerful!
-              </div>
-              <div className="quote-attribution">
-                Clark Daniels, The Food Company 
+              <div className="col-xs-12 col-sm-6 col-md-3">
+                <img className="logo-picture" src="https://deepfoods.com/wp-content/themes/deepfoods/assets/images/global/deep-foods.svg"/>
               </div>
             </div>
           </div>
         </div>
-        <div className="problem">
+        <div className="client__quote">
           <div className="container">
+            <div className="quote-text">
+              Modus is is best thing to happen to our brand. We have never been more confident and powerful!
+            </div>
+            <div className="quote-attribution">
+              Clark Daniels, The Food Company 
+            </div>
+          </div>
+        </div>
+        <div className="problem">
+          <div className="problem-text">
             <h1>Problem</h1>
             <h2>
               For most growing companies, the current bottoms-up planning methodology is controlled chaos in a series of spreadsheets.
             </h2>
           </div>
+          <div className="problem-picture"></div>
         </div>
         <div className="solution">
-          <div className="container">
+          <div className="solution-picture"></div>
+          <div className="solution-text">
             <h1>Solution</h1>
             <h2>
               Modus Planning offers a growing brand what it needs most: a scalable platform with capabilities that transcend far beyond the spreadsheet.
