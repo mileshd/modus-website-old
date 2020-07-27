df9e18abf2291b527d491494e63db3b3690f76a4
Miles Dickinson <miles.h.dickinson@gmail.com>
2020-07-27 09:54:22 -0400

    Add initial terms of service

--- src/pages/privacyPolicy.js
+++ src/pages/privacyPolicy.js
@@ -13,56 +13,43 @@ const PrivacyPolicyPage = ({ data, location }) => {
       <SEO title="Modus Planning Privacy Policy" />
       <div className="privacy-policy">
         <div className="container">
-          <h1>Modus Planning Privacy Policy</h1>
+          <h1>Privacy Policy</h1>
           <p>
             This privacy policy has been compiled to better serve those who are concerned with how their 'Personally Identifiable Information' (PII) is being used online. PII, as described in US privacy law and information security, is information that can be used on its own or with other information to identify, contact, or locate a single person, or to identify an individual in context. Please read our privacy policy carefully to get a clear understanding of how we collect, use, protect or otherwise handle your Personally Identifiable Information in accordance with our website.
           </p>
 
-          <h3>
+          <h2>
             What personal information do we collect from the people that visit our blog, website or app?
-          </h3>
+          </h2>
           <p>
             When ordering or registering on our site, as appropriate, you may be asked to enter your name, email address, mailing address, phone number, credit card information or other details to help you with your experience.
           </p>
 
-          <h3>
+          <h2>
             When do we collect information?
-          </h3>
+          </h2>
           <p>
             We collect information from you when you place an order, fill out a form, Open a Support Ticket or enter information on our site.
           </p>
 
-          <h3>
+          <h2>
             How do we use your information?
-          </h3>
+          </h2>
           <p>
             We may use the information we collect from you when you register, make a purchase, sign up for our newsletter, respond to a survey or marketing communication, surf the website, or use certain other site features in the following ways:
           </p>
           <ul>
-            <li>
-              To personalize your experience and to allow us to deliver the type of content and product offerings in which you are most interested.
-            </li>
-            <li>
-              To improve our website in order to better serve you.
-            </li>
-            <li>
-              To allow us to better service you in responding to your customer service requests.
-            </li>
-            <li>
-              To quickly process your transactions.
-            </li>
-            <li>
-              To send periodic emails regarding your order or other products and services.
-            </li>
-            <li>
-              To follow up with them after correspondence (live chat, email or phone inquiries)
-            </li>
+            <li>To personalize your experience and to allow us to deliver the type of content and product offerings in which you are most interested.</li>
+            <li>To improve our website in order to better serve you.</li>
+            <li>To allow us to better service you in responding to your customer service requests.</li>
+            <li>To quickly process your transactions.</li>
+            <li>To send periodic emails regarding your order or other products and services.</li>
+            <li>To follow up with them after correspondence (live chat, email or phone inquiries)</li>
           </ul>
-
     
-          <h3>
+          <h2>
             How do we protect your information?
-          </h3>
+          </h2>
           <p>
             Our website is scanned on a regular basis for security holes and known vulnerabilities in order to make your visit to our site as safe as possible.
           </p>
@@ -76,16 +63,16 @@ const PrivacyPolicyPage = ({ data, location }) => {
             All transactions are processed through a gateway provider and are not stored or processed on our servers.
           </p>
     
-          <h3>
+          <h2>
             Do we use 'cookies'?
-          </h3>
+          </h2>
           <p>
             Yes. Cookies are small files that a site or its service provider transfers to your computer's hard drive through your Web browser (if you allow) that enables the site's or service provider's systems to recognize your browser and capture and remember certain information. For instance, we use cookies to help us remember and process the items in your shopping cart. They are also used to help us understand your preferences based on previous or current site activity, which enables us to provide you with improved services. We also use cookies to help us compile aggregate data about site traffic and site interaction so that we can offer better site experiences and tools in the future.
           </p>
 
-          <h3>
+          <h2>
             Third-party disclosure
-          </h3>
+          </h2>
           <p>
             We do not sell, trade, or otherwise transfer to outside parties your Personally Identifiable Information unless we provide users with advance notice. This does not include website hosting partners and other parties who assist us in operating our website, conducting our business, or serving our users, so long as those parties agree to keep this information confidential. We may also release information when it's release is appropriate to comply with the law, enforce our site policies, or protect ours or others' rights, property or safety.
           </p>
@@ -93,17 +80,17 @@ const PrivacyPolicyPage = ({ data, location }) => {
             However, non-personally identifiable visitor information may be provided to other parties for marketing, advertising, or other uses.
           </p>
 
-          <h3>
+          <h2>
             Third-party links
-          </h3>
+          </h2>
           <p>
             Occasionally, at our discretion, we may include or offer third-party products or services on our website. These third-party sites have separate and independent privacy policies. We therefore have no responsibility or liability for the content and activities of these linked sites. Nonetheless, we seek to protect the integrity of our site and welcome any feedback about these sites.
           </p>
 
 
-          <h3>
+          <h2>
             California Online Privacy Protection Act
-          </h3>
+          </h2>
           <p>
             CalOPPA is the first state law in the nation to require commercial websites and online services to post a privacy policy. The law's reach stretches well beyond California to require any person or company in the United States (and conceivably the world) that operates websites collecting Personally Identifiable Information from California consumers to post a conspicuous privacy policy on its website stating exactly the information being collected and those individuals or companies with whom it is being shared.
           </p>
@@ -121,9 +108,9 @@ const PrivacyPolicyPage = ({ data, location }) => {
           </ul>
 
 
-          <h3>
+          <h2>
             COPPA (Children Online Privacy Protection Act)
-          </h3>
+          </h2>
           <p>
             When it comes to the collection of personal information from children under the age of 13 years old, the Children's Online Privacy Protection Act (COPPA) puts parents in control. The Federal Trade Commission, United States' consumer protection agency, enforces the COPPA Rule, which spells out what operators of websites and online services must do to protect children's privacy and safety online.
           </p>
@@ -132,20 +119,19 @@ const PrivacyPolicyPage = ({ data, location }) => {
           </p>
 
           
-          <h3>
+          <h2>
             Fair Information Practices
-          </h3>
+          </h2>
           <p>
             The Fair Information Practices Principles form the backbone of privacy law in the United States and the concepts they include have played a significant role in the development of data protection laws around the globe. Understanding the Fair Information Practice Principles and how they should be implemented is critical to comply with the various privacy laws that protect personal information.
           </p>
           <p>
             In order to be in line with Fair Information Practices we will take the following responsive action, should a data breach occur, we will notify you via email within 7 business days.
           </p> 
-
           
-          <h3>
+          <h2>
             CAN SPAM Act
-          </h3>
+          </h2>
           <p>
             The CAN-SPAM Act is a law that sets the rules for commercial email, establishes requirements for commercial messages, gives recipients the right to have emails stopped from being sent to them, and spells out tough penalties for violations.
           </p>
@@ -153,33 +139,39 @@ const PrivacyPolicyPage = ({ data, location }) => {
             We collect your email address in order to:
           </p>
           <ul>
-            <li>
-              Send information, respond to inquiries, and/or other requests or questions
-            </li>
-            <li>
-              Process orders and to send information and updates pertaining to orders.
-            </li>
-            <li>
-              Send you additional information related to your product and/or service
-            </li>
+            <li>Send information, respond to inquiries, and/or other requests or questions</li>
+            <li>Process orders and to send information and updates pertaining to orders.</li>
+            <li>Send you additional information related to your product and/or service</li>
           </ul>
           <p>
             To be in accordance with CANSPAM, we agree to the following:
           </p>
           <ul>
-            <li>Not use false or misleading subjects or email addresses.</li>
+            <li>Not use false or misleading subjects or email addresses.</li>
             <li>Identify the message as an advertisement in some reasonable way.</li>
-
             <li>Include the physical address of our business or site headquarters.</li>
-            <li>Monitor third-party email marketing services for compliance, if one is used.</li>
-            <li>Honor opt-out/unsubscribe requests quickly.</li>
-            <li>Allow users to unsubscribe by using the link at the bottom of each email.</li>
-      <li> If at any time you would like to unsubscribe from receiving future emails, you can email us at
-      info@onepagesoftware.com and we will promptly remove you from ALL correspondence.</li>
+            <li>Monitor third-party email marketing services for compliance, if one is used.</li>
+            <li>Honor opt-out/unsubscribe requests quickly.</li>
+            <li>Allow users to unsubscribe by using the link at the bottom of each email.</li>
+            <li>
+              If at any time you would like to unsubscribe from receiving future emails, you can email us at info@onepagesoftware.com and we will promptly remove you from ALL correspondence.
+            </li>
           </ul>
+
+          <h2>
+            Contacting Us
+          </h2>
+          <p>
+            If there are any questions regarding this privacy policy, you may contact us using the information below.
+          </p>
+          <div>onepagesoftware.com</div>
+          <div>248 Elkins Ln</div>
+          <div>Lusby, MD 20657</div>
+          <div>United States</div>
+          <div><b>info@onepagesoftware.com</b></div>
+          <div><b>301-752-0016</b></div>
         </div>
       </div>
-
     </Layout>
   )
 }
--- src/pages/termsOfService.js
+++ src/pages/termsOfService.js
@@ -13,7 +13,7 @@ const TermsOfServicePage = ({ data, location }) => {
       <SEO title="Modus Planning Terms of Service" />
       <div className="terms-of-service">
         <div className="container">
-          <h1>Modus Planning Website Terms Of Service</h1>
+          <h1>Terms Of Service</h1>
 
           <h2>License to use Website</h2>
           <p>
@@ -89,6 +89,130 @@ const TermsOfServicePage = ({ data, location }) => {
           </p>
 
           <h2>No Warranties</h2>
+          <p>
+            This website is provided "as is" without any representations or warranties, express or implied.  OnePage Software, LLC. makes no representations or warranties in relation to this website or the information and materials provided on this website.  
+          </p>
+          <p>
+            Without prejudice to the generality of the foregoing paragraph, OnePage Software, LLC. does not warrant that:
+          </p>
+          <ul>
+            <li>
+              This website will be constantly available, or available at all; or
+            </li>
+            <li>
+              The information on this website is complete, true, accurate or non-misleading despite OnePage Software, LLC’s best efforts.
+            </li>
+          </ul>
+          <p>
+            Nothing on this website constitutes, or is meant to constitute, advice of any kind. If you require advice in relation to any matter you should consult an appropriate professional.
+          </p>
+
+          <h2>Limitation of Liability</h2>
+          <p>
+            OnePage Software, LLC. will not be liable to you (whether under the law of contact, the law of torts or otherwise) in relation to the contents of, or use of, or otherwise in connection with, this website:
+          </p>
+          <ul>
+            <li>
+              for any direct loss;
+            </li>
+            <li>
+              for any indirect, special or consequential loss; or
+            </li>
+            <li>
+              for any business losses, loss of revenue, income, profits or anticipated savings, loss of contracts or business relationships, loss of reputation or goodwill, or loss or corruption of information or data.
+            </li>
+          </ul>
+          <p>
+            These limitations of liability apply even if OnePage Software, LLC. has been expressly advised of the potential loss.
+          </p>
+
+          <h2>Exceptions</h2>
+          <p>
+            Nothing in this website disclaimer will exclude or limit any warranty implied by law that it would be unlawful to exclude or limit; and nothing in this website disclaimer will exclude or limit OnePage Software, LLC. liability in respect of any:
+          </p>
+          <ul>
+            <li>
+              death or personal injury caused by OnePage Software, LLC. negligence;
+            </li>
+            <li>
+              fraud or fraudulent misrepresentation on the part of OnePage Software, LLC.; or
+            </li>
+            <li>
+              matter which it would be illegal or unlawful for OnePage Software, LLC. to exclude or limit, or to attempt or purport to exclude or limit, its liability. 
+            </li>
+          </ul>
+
+          <h2>Reasonableness</h2>
+          <p>
+            By using this website, you agree that the exclusions and limitations of liability set out in this website disclaimer are reasonable.
+          </p>
+          <p>
+            If you do not think they are reasonable, you must not use this website.
+          </p>
+
+          <h2>Other parties</h2>
+          <p>
+            You accept that, as a limited liability entity, OnePage Software, LLC. has an interest in limiting the personal liability of its officers and employees.  You agree that you will not bring any claim personally against OnePage Software, LLC. officers or employees in respect of any losses you suffer in connection with the website.
+          </p>
+          <p>
+            Without prejudice to the foregoing paragraph, you agree that the limitations of warranties and liability set out in this website disclaimer will protect OnePage Software, LLC. officers, employees, agents, subsidiaries, successors, assigns and sub-contractors as well as OnePage Software, LLC..
+          </p>
+
+          <h2>Unenforceable provisions</h2>
+          <p>
+            If any provision of this website disclaimer is, or is found to be, unenforceable under applicable law, that will not affect the enforceability of the other provisions of this website disclaimer.
+          </p>
+
+          <h2>Indemnity</h2>
+          <p>
+            You hereby indemnify OnePage Software, LLC. and undertake to keep OnePage Software, LLC. indemnified against any losses, damages, costs, liabilities and expenses (including without limitation legal expenses and any amounts paid by OnePage Software, LLC. to a third party in settlement of a claim or dispute on the advice of OnePage Software, LLC. legal advisers) incurred or suffered by OnePage Software, LLC. arising out of any breach by you of any provision of these terms and conditions, or arising out of any claim that you have breached any provision of these terms and conditions.
+          </p>
+
+          <h2>Breaches of these terms and conditions</h2>
+          <p>
+            Without prejudice to OnePage Software, LLC. other rights under these terms and conditions, if you breach these terms and conditions in any way, OnePage Software, LLC. may take such action as OnePage Software, LLC. deems appropriate to deal with the breach, including suspending your access to the website, prohibiting you from accessing the website, blocking computers using your IP address from accessing the website, contacting your internet service provider to request that they block your access to the website and/or bringing court proceedings against you.
+          </p>
+
+          <h2>Variation</h2>
+          <p>
+            OnePage Software, LLC. may revise these terms and conditions from time-to-time.  Revised terms and conditions will apply to the use of this website from the date of the publication of the revised terms and conditions on this website. Please check this page regularly to ensure you are familiar with the current version.
+          </p>
+
+          <h2>Assignment</h2>
+          <p>
+            OnePage Software, LLC. may transfer, sub-contract or otherwise deal with OnePage Software, LLC. rights and/or obligations under these terms and conditions without notifying you or obtaining your consent.
+          </p>
+          <p>
+            You may not transfer, sub-contract or otherwise deal with your rights and/or obligations under these terms and conditions.  
+          </p>
+
+          <h2>Severability</h2>
+          <p>
+            If a provision of these terms and conditions is determined by any court or other competent authority to be unlawful and/or unenforceable, the other provisions will continue in effect.  If any unlawful and/or unenforceable provision would be lawful or enforceable if part of it were deleted, that part will be deemed to be deleted, and the rest of the provision will continue in effect. 
+          </p>
+
+          <h2>Entire Agreement</h2>
+          <p>
+            These terms and conditions constitute the entire agreement between you and OnePage Software, LLC. in relation to your use of this website, and supersede all previous agreements in respect of your use of this website.
+          </p>
+
+          <h2>Law and Jurisdiction</h2>
+          <p>
+            These terms and conditions will be governed by and construed in accordance with laws of the State of Maryland and any disputes relating to these terms and conditions will be subject to the [non-]exclusive jurisdiction of the courts of the State of Maryland.
+          </p>
+
+          <h2>
+            Contacting Us
+          </h2>
+          <p>
+            If there are any questions regarding these terms of service, you may contact us using the information below.
+          </p>
+          <div>onepagesoftware.com</div>
+          <div>248 Elkins Ln</div>
+          <div>Lusby, MD 20657</div>
+          <div>United States</div>
+          <div><b>info@onepagesoftware.com</b></div>
+          <div><b>301-752-0016</b></div>
         </div>
       </div>
     </Layout>
